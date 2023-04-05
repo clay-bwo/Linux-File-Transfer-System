bb7dc5dcfc95170a5e17826f25f2826d98329215
jmayrides <jmayride@villanova.edu>
2023-04-04 21:41:29 -0400

    Push Jack local changes

--- client.py
+++ client.py
@@ -1,9 +1,45 @@
-#!/usr/bin/env python3
 import socket
-HOST = '127.0.0.1' # The server's hostname or IP address
+import rsa
+
+HOST = '127.0.0.1' # The server's IP address
 PORT = 65432 # The port used by the server
+
+# Generate set of keys
+client_public_key, private_key = rsa.newkeys(512)
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-    print('Received', repr(data))
+
+    # Send client's public key to server
+    s.sendall(client_public_key.save_pkcs1())
+
+    while True:
+        # Get file path from user
+        file_path = input('Enter file path (or "exit" to quit): ')
+        if file_path == 'exit':
+            break
+
+        # Send inputted file path to server
+        s.sendall(file_path.encode())
+
+        # Receive encrypted file data from server
+        encrypted_file_data = s.recv(1024)
+
+        # Check for error message
+        if encrypted_file_data.startswith(b'Error'):
+            print(encrypted_file_data.decode())
+        else:
+            # Decrypt file data using private key
+            file_data = rsa.decrypt(encrypted_file_data, private_key)
+
+        # Get file name from file path
+        file_name = file_path.split('/')[-1]
+
+        # Write the decrypted file data to a new file
+        with open("received_file.txt", 'wb') as file:
+            file.write(file_data)
+
+        print(f'Received file: {file_name} from server')
+
+    # Close the client socket
+    s.close()
--- server.py
+++ server.py
@@ -1,17 +1,57 @@
-#!/usr/bin/env python3
 import socket
+import os
+import threading
+import rsa
+
 HOST = '127.0.0.1' # Standard loopback interface address (localhost)
 PORT = 65432 # Port to listen on (non-privileged ports are > 1023)
 
+# Function to handle client requests
+def handle_client(client_socket, client_addr):
+
+    # Receive client's public key
+    client_public_key = rsa.PublicKey.load_pkcs1(client_socket.recv(1024))
+
+    while True:
+        # Receive file path from client
+        file_path = client_socket.recv(1024).decode()
+
+        if not file_path:
+            break
+
+        # Check if file exists
+        if os.path.exists(file_path):
+            # If so, open the file in binary mode
+            with open(file_path, 'rb') as file:
+                # Read the file contents
+                file_data = file.read()
+
+            # Encrypt file data using client's public key
+            encrypted_file_data = rsa.encrypt(file_data, client_public_key)
+
+            # Send encrypted file contents to the client
+            client_socket.sendall(encrypted_file_data)
+            print(f'Sent file: {file_path} to {client_addr}')
+        else:
+            # Send error message to client
+            error_msg = f'Error: File not found: {file_path}'.encode()
+            client_socket.sendall(error_msg)
+            print(f'Error: File not found: {file_path} for {client_addr}')
+
+    # Close the client socket
+    client_socket.close()
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # Bind the socket to the host and port, and listen for incoming connections
     s.bind((HOST, PORT))
     s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+    print(f'Server listening on [{HOST}]:{PORT}...')
+
+    while True:
+        # Accept a client connection
+        client_socket, client_addr = s.accept()
+        print(f'Connection from {client_addr}')
+
+        # Start a new thread to handle client request
+        thread = threading.Thread(target=handle_client, args=(client_socket, client_addr))
+        thread.start()
